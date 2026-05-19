dfa8105864739448abd48996a00cfeb518b64b44
paulbardar <Q.C.tulip@gmail.com>
2026-05-19 19:08:46 +0300

    Music 2

--- 38_CW/Music.h
+++ 38_CW/Music.h
@@ -3,6 +3,16 @@
 #include <iostream>
 class Music
 {
+	std::string name;
+	std::string author;
+	int year;
+
+public: 
+	Music(std::string name, std::string author, int year);
+
+	std::string getName() const;
+	std::string getAuthor() const;
+	int getYear() const;
 
 
 };
